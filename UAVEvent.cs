a9b47f5cc5a278cd253f910a523d369076dfbf14
motagota <motagota@gmail.com>
2011-04-28 14:51:18 +1000

    Added UAVEvent

---
+++ UAVEvent.cs
@@ -0,0 +1,11 @@
+public class UAVEvent
+{
+   Command commmand;
+   int value;
+   
+   public UAVEvent(Commmand command, Value value)
+   {
+      this.command = commmand;
+	  this.value = value;
+   }
+}
